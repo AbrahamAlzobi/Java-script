991d0392d8a5a3ae0040fc2ca5da6592b97f6e0a
AbrahamAlzobi <a.alzobi@share.epsb.ca>
2018-09-21 12:46:57 -0600

    update

--- swapping variables/index.js
+++ swapping variables/index.js
@@ -1,26 +1,14 @@
-console.log("Hello World");
 
-// create 10 variable about yourself
 let first name = "Abraham";
 let last name = "Alzo;bi";
-let age = "14";
-let hair = "brown";
-let teeth = "braces";
-let fav subject = "science";
-let hobby = "video games":
-let height = "5'7";
-const ageAfraid = 14; // security feature
-let fact = "likes computers";
-let gender = "male";
+//new code goes here
+let first name = "Mark";
+let last name = "Mercer"
+console.log firstname = (Abraham)
+console.log lastname = (Alzobi)
+
 
 
 // print variabls to console
 console.log(firstname);
 console.log(lastname);
-console.log(age);
-console.log(hair);
-console.log(teeth);
-console.log(favSubject);
-console.log(hobby);
-console.log(fact);
-console.log(gender);
