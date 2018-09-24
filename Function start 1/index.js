c148df5ad04d2dc319be548a95fc514b2a5451d2
Abraham Alzo'bi <a.alzobi@EDU.epsb.ca>
2018-09-24 13:55:31 -0600

    update

---
+++ Function start 1/index.js
@@ -0,0 +1,22 @@
+// hello world function
+function greet (firstName, lastName);{ // parameters
+  console.log("hello"+  "World" + lastName)
+}
+
+function square (number) {
+  return number*number;
+}
+
+
+// your function goes here
+
+
+
+// firstName, lastName, age, hair, legs, fav subject science,ageAfraid, male, car, height, female,
+let AbrahamAlzobi = ["Abraham", "alzobi", 14, true, true, true, true, null, true, null, 5 7, false,];
+let markmercer = ["Mark", "mercer" 13, 29, null, true, false]
+
+// calling functions, with arguments
+greet (AbrahamAlzobi[0], AbrahamAlzobi [1]);
+greet (markmercer[0], markmercer[1]);
+greet (square(AbrahamAlzobi[2]), markmercer[2]);
