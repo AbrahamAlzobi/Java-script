fffdb1451328bcbc12e84f87e95d3d6cbea46037
AbrahamAlzobi <a.alzobi@share.epsb.ca>
2018-10-11 13:21:44 -0600

    update

--- FizzBuzz/index.js
+++ FizzBuzz/index.js
@@ -1,10 +1,9 @@
-const testNumber = 15
-console.log(fizzBuzz(15)); //Calling Function
-console.log(fizzBuzz(10));
+const testNumber = 10;
+console.log(fizzBuzz(testNumber)); //Calling Function
 function fizzBuzz () { //These match order of logic
-  if (15 % 3 === 0) return "Fizz";
-  if (10 % 5 === 0) return 'Buzz';
-  if ((15 % 3 === 0 ) && (10 % 5 === 0)) return 'FizzBuss';
+  if ((testNumber % 3 === 0 ) && (testNumber % 5 === 0)) return 'FizzBuss';
+  if (testNumber % 5 === 0) return 'Buzz';
+  if (testNumber % 3 === 0) return "Fizz";
   if (typeof input !=='number') return 'This is not a number.'; //Change to NaN
   return "This test number does nothing important. The input was " + input + ".";
 }
---
+++ greeting/index.js
@@ -0,0 +1,2 @@
+let hour = 06 || 11; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
+if (hour >= 06); console.log('Good Morning');
